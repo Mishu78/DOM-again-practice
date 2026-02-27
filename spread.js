4ee3cb2e5fbfafeff39a64a8a122c43a9bd0cd14
programming hero <meherun2002@gmail.com>
2026-02-27 12:31:13 +0600

    spread.js added

---
+++ spread.js
@@ -0,0 +1,6 @@
+const numbers=Math.max(23,45,50,12,34,47,58,10,52,63);
+console.log(numbers);
+
+const maximum = [29,48,28,58,204,48454,57985,302,4874,57,484,2948,575,58,57,5758];
+const max2 = Math.max(...maximum);
+console.log(max2);
