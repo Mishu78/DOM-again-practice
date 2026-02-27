1c8a152840a63d50281bcd06098af98e74e570a1
programming hero <meherun2002@gmail.com>
2026-02-27 14:38:16 +0600

    optional chaining added

--- braket_notation.js
+++ braket_notation.js
@@ -1,2 +1,15 @@
-const identity = {name:'mishu',1:"desk-1",age:25,id:221014038,des:"i am a good little girl" };
-console.log(identity[1]);
+const identity = {name:'mishu',
+    1:"desk-1",
+    age:25,
+    id:221014038,
+    des:"i am a good little girl",
+    family:{
+        father:"abdul matin",
+        mother:{
+            name:"rehena",
+            age:60,
+        }
+    } };
+//console.log(identity[1]);
+console.log(identity.family.mother?.work);
+
