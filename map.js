7d74d2f57497b1bf368e9192f913558e1daecd4e
programming hero <meherun2002@gmail.com>
2026-03-01 20:16:30 +0600

    mapping used for double and square number

---
+++ map.js
@@ -0,0 +1,10 @@
+const numbers =[1,2,3,4,5];
+// const doubled = [];
+// for(const number of numbers){
+//   doubled.push(number * 2);
+// }
+const doubleIt = num => num*2;
+const doubled = numbers.map(doubleIt);
+const square = numbers.map(num => num*num);
+console.log(doubled);
+console.log(square);
