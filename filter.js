f34454a5c93ad9ef68f4ae2896221b655ab69498
Mishu78 <meherun2002@gmail.com>
2026-03-02 10:08:10 +0600

    older students added by filtering

--- filter.js
+++ filter.js
@@ -5,4 +5,16 @@ console.log(evenNumbers);
 console.log(oddNumbers);
 const friends =['mishu','naved','khaled','sajed','nahid','nasib'];
 const nFriends=friends.filter(name=>name[0]==='n');
+
+const students=[
+    {name:'mishu',age:25},
+    {name:'sapa',age:24},
+    {name:'naved',age:20},
+    {name:'javed',age:35},
+    {name:'khaled',age:45},
+    {name:'sajed',age:15},
+];
 console.log(nFriends);
+const olderStudents=students.filter(std=>std.age>30);
+console.log(olderStudents);
+;
